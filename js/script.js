a56af3a5205a27c8f21b3c00123c519692332a31
MaksBmt <solov@tdpsm.ru>
2019-08-31 18:30:08 +0300

    новые изменения

--- js/script.js
+++ js/script.js
@@ -8,4 +8,57 @@ $(document).ready(function () {
     $(this).parents('.works__element').removeClass('works__element--active');
   });
 
+/*//восстанавливает линию в зависимости от величины в процентах
+    //node - класс, где присутствует data атрибут
+    //nodeLine - класс блока линии
+    //tooltip - класс для подсказки
+    //animationLength - скорость анимации
+function moveProgressBar(node, nodeLine, tooltip, animationLength = 1500) {
+    const progressElement = $(node);
+    progressElement.each(function (value, item) { //перебираем каждый элемент при помощи each,
+      $(item).find(nodeLine).animate({            //внутри каждого элемента item при помощи функции find ищем класс линии и применяем к ней animate
+        width: item.dataset.progressPercent+'%'   //где анимируем величину ширины используя dataset и константу progressElement
+      }, animationLength);
+      $(item).find(tooltip).show(animationLength); //показываем через 1.5 с подсказку: ищем ee функцией find и использую show показываем
+    });
+  }
+
+  let animate = true;
+
+
+  $(window).scroll(function () {
+
+    if($('.skills').offset().top <= $(window).scrollTop() + 150) {
+      if(animate) {
+          
+          //вызываем функцию
+        moveProgressBar('.progress__element', '.progress__line', '.progress__tooltip');
+      }
+      animate = false;
+    }
+
+  });*/
+ function moveProgressBar(node, nodeLine, tooltip, animationLength = 1500) {
+    const progressElement = $(node);
+    progressElement.each(function (value, item) {
+      $(item).find(nodeLine).animate({
+        width: item.dataset.progressPercent+'%'
+      }, animationLength);
+      $(item).find(tooltip).show(animationLength);
+    });
+  }
+
+  let animate = true;
+
+
+  $(window).scroll(function () {
+
+    if($('.skills').offset().top <= $(window).scrollTop() + 150) {
+      if(animate) {
+        moveProgressBar('.progress__element', '.progress__line', '.progress__tooltip');
+      }
+      animate = false;
+    }
+
+  });
 });
